3f7483f5abb0b3df61cce0d781abb218afc08a72
SravanthiM027 <143540133+SravanthiM027@users.noreply.github.com>
2024-01-08 12:45:34 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const app = express();
+const mongoose =require('mongoose')
+const cors = require('cors')
+
+app.get('/',(req,res)=>{res.send("hello from server..")})
+
+app.use(cors())
+app.use(express.json())
+
+const Db = async()=>{
+    try {
+        await mongoose.connect('mongodb+srv://MEDICHARLASRAVANTHI:sravanthi@test-pro-db.62qpvzg.mongodb.net/sasi?retryWrites=true&w=majority').then(
+            ()=>{
+                console.log("Connected to DataBase...")
+            }
+        )
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+Db();
+
+app.listen(3000,()=>{console.log("Server is running on port 3000...")})
